9d4514bf5c5e180ee471045995d028cb3aa82e77
StudentPP1 <mishamakytonin@gmail.com>
2024-05-14 00:36:21 +0300

    delete notes

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ from nltk.corpus import stopwords
 from torch import optim, nn
 from gpt import GPT
 
-"""
-Materials:
-preprocessing text - https://medium.com/@pawan329/text-data-preprocessing-made-easy-steps-to-clean-text-data-using-python-81a138a0e0e3
-basic model - https://medium.com/@sntaus/building-a-mini-gpt-like-language-model-from-scratch-27257bf5c145
-dataset - https://rajpurkar.github.io/SQuAD-explorer/
-"""
-
 training_data = {}
 
 
